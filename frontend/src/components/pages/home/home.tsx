7ce205a432527083192299785b65348e39e3df48
Michal Leibovich <michalleibovich1@gmail.com>
2026-01-27 15:08:53 +0200

    created home page with btns and stuff haha

--- frontend/src/components/pages/home/home.tsx
+++ frontend/src/components/pages/home/home.tsx
@@ -1,17 +1,28 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import useStyles from "./homeStyles";
 import { NavLink } from 'react-router-dom';
-
+import AddIcon from '@mui/icons-material/Add';
 
 const Home: React.FC = () => {
     const { classes, cx } = useStyles();
-    return(
+    return (
         <div className={classes.wholeHomeContainer}>
-            <div>
-                <Button className={classes.pagesButton} component={NavLink} to="/allDebriefings">כל התחקירים</Button>
-                <Button className={classes.pagesButton} component={NavLink} to="/recentDebriefings">תחקירים אחרונים</Button>
+            <div className={classes.allPartsContainer}>
+                <Typography variant="h3" className={classes.text}>שלום User<br/>מה ברצונך לעשות?</Typography>
+
+                <div className={classes.buttonsContainer}>
+                    <Button className={classes.pagesButton} component={NavLink} to="/newDebriefing">
+                        <AddIcon className={classes.icon}/>
+                        <Typography variant="h5" className={classes.text}>תחקיר חדש</Typography>
+                    </Button>
+
+                    <div className={classes.leftButtonsContainer}>
+                        <Button className={classes.pagesButton} component={NavLink} to="/allDebriefings">כל התחקירים</Button>
+                        <Button className={classes.pagesButton} component={NavLink} to="/recentDebriefings">תחקירים אחרונים</Button>
+                    </div>
+                </div>
             </div>
-            
+
         </div>
     )
 }
--- frontend/src/components/pages/home/homeStyles.ts
+++ frontend/src/components/pages/home/homeStyles.ts
@@ -4,12 +4,66 @@ const useStyles = makeStyles()(() => ({
     wholeHomeContainer: {
         display: "flex",
         flex: 1,
+        justifyContent: "center",
         width: "100%",
         backgroundColor: "#0F1027"
     },
+
+
+    allPartsContainer: {
+        marginTop: "40px",
+        display: "flex",
+        // backgroundColor: "yellow",
+        flexDirection: "column",
+        gap: "20vh",
+        color: "white"
+    },
+
+
+    buttonsContainer: {
+        display: "flex",
+        gap: "50px",
+        // backgroundColor: "rgb(23, 29, 195)"
+    },
+
+    icon: {
+        fontSize: "50px"
+    },
+    text: {
+        fontFamily: '"Noto Sans Hebrew", sans-serif',
+        fontSize: "22px",
+        textAlign: "center",
+    },
+
+    leftButtonsContainer: {
+        display: "flex",
+        gap: "20px",
+        // backgroundColor: "rgb(23, 29, 195)"
+    },
+
     pagesButton: {
-        
+        display: "flex",
+        flexDirection: "column",
+        borderRadius: "30px",
+        color: "white",
+        fontFamily: '"Noto Sans Hebrew", sans-serif',
+        height: "170px",
+        width: "170px",
+        fontSize: "22px",
+        textAlign: "center",
+        backgroundColor: "#6F0F46",
+        boxShadow: "inset 0 0 20px rgba(0, 0, 0, 0.45)",
+        "&:hover": {
+            color: "white",
+            boxShadow: "inset 0 0 25px rgba(0, 0, 0, 0.6)",
+        },
+        // "& ,fading-border-element": {
+        //     "&,fading-shadow-element": {
+        //         boxShadow: "0 0 10px 5px rgba(0, 0, 0, 0.52)" /* Fades a black shadow outwards */
+        //     }
+        // }
     }
+
 }));
 
 export default useStyles;
