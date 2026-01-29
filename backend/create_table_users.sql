c012e8edac4243d2809094c94e0e7d77fb847579
Michal Leibovich <michalleibovich1@gmail.com>
2026-01-29 17:02:24 +0200

    Changed MyAccount page to save data in DB!

---
+++ backend/create_table_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE debriefing_project.users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
