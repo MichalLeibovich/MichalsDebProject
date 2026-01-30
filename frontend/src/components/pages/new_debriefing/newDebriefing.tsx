8a1ef5ddc312647b25c986380f12f4a1adcdd852
Michal Leibovich <michalleibovich1@gmail.com>
2026-01-30 07:04:37 +0200

    did half of the work in new debriefing page

--- frontend/src/components/pages/new_debriefing/newDebriefing.tsx
+++ frontend/src/components/pages/new_debriefing/newDebriefing.tsx
@@ -1,8 +1,34 @@
 import React, { useState, type FormEvent } from 'react';
 import axios from 'axios';
+import useStyles from './newDebriefingStyles';
+import { Typography } from '@mui/material';
+
+type PersonInvolved = {
+  id: number;
+  name: string;
+  phone: string;
+};
 
 const NewDebriefing: React.FC = () => {
+  const { classes, cx } = useStyles();
   const [title, setTitle] = useState("");
+  const [documentFillerName, setDocumentFillerName] = useState("");
+  const [personalNumber, setPersonalNumber] = useState("");
+  const [date, setDate] = useState("");
+  const [errorDealers, setErrorDealers] = useState("");
+
+
+  const [errorDiscoverers, setErrorDiscoverers] = useState<PersonInvolved[]>([
+    { id: 0, name: "", phone: "" }
+  ]);
+  const [errorSolverers, setErrorSolverers] = useState<PersonInvolved[]>([
+    { id: 0, name: "", phone: "" }
+  ]);
+
+  const [teams, setTeams] = useState<string[]>([]);
+
+  const [errorDescription, setErrorDescription] = useState("");
+
   const [message, setMessage] = useState("");
 
   const handleSubmit = async (e: FormEvent) => {
@@ -14,7 +40,7 @@ const NewDebriefing: React.FC = () => {
       });
       setMessage(`Created debriefing: ${response.data.title}`);
       setTitle("");
-    } 
+    }
     catch (error) {
       console.error('There was an error saving the data!', error);
       setMessage('Error saving data.' + error);
@@ -22,18 +48,284 @@ const NewDebriefing: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Save Data to SQL Database</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Title:
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
-          </label>
-        </div>
-        <button type="submit">Save Data</button>
-      </form>
-      {message && <p>{message}</p>}
+    <div className={classes.wholePageContainer}>
+      <div className={classes.allPartsContainer}>
+        <Typography variant="h4" className={classes.text}>תחקיר חדש</Typography>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">כותרת:</label>
+            <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+          </div>
+
+          <div>
+            <Typography variant="h4">פרטים כלליים</Typography>
+            <div>
+              <label htmlFor="title">שם ממלא המסמך:</label>
+              <input id="title" type="text" value={documentFillerName} onChange={(e) => setDocumentFillerName(e.target.value)} required />
+            </div>
+            <div>
+              <label htmlFor="title">מ.א:</label>
+              <input id="title" type="number" value={personalNumber} onChange={(e) => setPersonalNumber(e.target.value)} required />
+            </div>
+            <div>
+              <label htmlFor="title">תאריך:</label>
+              <input id="title" type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+            </div>
+          </div>
+
+          <div>
+            <Typography variant="h4">הגורמים המשתתפים</Typography>
+            <div>
+              <label htmlFor="title">מנהלי התקלה מצוות נוק:</label>
+              <input id="title" type="text" value={errorDealers} onChange={(e) => setErrorDealers(e.target.value)} required />
+            </div>
+            <div>
+              <Typography variant="h6">מגלה התקלה</Typography>
+
+              {errorDiscoverers.map((person) => (
+                <div key={person.id} style={{ marginBottom: "16px" }}>
+                  <div>
+                    <label>שם מלא:</label>
+                    <input
+                      type="text"
+                      value={person.name}
+                      onChange={(e) =>
+                        setErrorDiscoverers((prev) =>
+                          prev.map((p) =>
+                            p.id === person.id
+                              ? { ...p, name: e.target.value }
+                              : p
+                          )
+                        )
+                      }
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label>מספר טלפון:</label>
+                    <input
+                      type="number"
+                      value={person.phone}
+                      onChange={(e) =>
+                        setErrorDiscoverers((prev) =>
+                          prev.map((p) =>
+                            p.id === person.id
+                              ? { ...p, phone: e.target.value }
+                              : p
+                          )
+                        )
+                      }
+                      required
+                    />
+                  </div>
+                </div>
+              ))}
+
+              <button
+                type="button"
+                onClick={() =>
+                  setErrorDiscoverers((prev) => [
+                    ...prev,
+                    { id: Date.now(), name: "", phone: "" }
+                  ])
+                }
+              >
+                +
+              </button>
+            </div>
+
+            <div>
+              <Typography variant="h6">פותר התקלה</Typography>
+
+              {errorSolverers.map((person) => (
+                <div key={person.id} style={{ marginBottom: "16px" }}>
+                  <div>
+                    <label>שם מלא:</label>
+                    <input
+                      type="text"
+                      value={person.name}
+                      onChange={(e) =>
+                        setErrorSolverers((prev) =>
+                          prev.map((p) =>
+                            p.id === person.id
+                              ? { ...p, name: e.target.value }
+                              : p
+                          )
+                        )
+                      }
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label>מספר טלפון:</label>
+                    <input
+                      type="number"
+                      value={person.phone}
+                      onChange={(e) =>
+                        setErrorSolverers((prev) =>
+                          prev.map((p) =>
+                            p.id === person.id
+                              ? { ...p, phone: e.target.value }
+                              : p
+                          )
+                        )
+                      }
+                      required
+                    />
+                  </div>
+                </div>
+              ))}
+
+              <button
+                type="button"
+                onClick={() =>
+                  setErrorSolverers((prev) => [
+                    ...prev,
+                    { id: Date.now(), name: "", phone: "" }
+                  ])
+                }
+              >
+                +
+              </button>
+            </div>
+
+            <div>
+              <label>
+                <input
+                  type="checkbox"
+                  value="butterfly-effect"
+                  checked={teams.includes("אפקט הפרפר")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                אפקט הפרפר
+              </label>
+              <br />
+              <label>
+                <input
+                  type="checkbox"
+                  value="gaussian"
+                  checked={teams.includes("גאוסיין")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                גאוסיין
+              </label>
+              <br />
+              <label>
+                <input
+                  type="checkbox"
+                  value="harmony"
+                  checked={teams.includes("הרמוניה")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                הרמוניה
+              </label>
+              <br />
+              <label>
+                <input
+                  type="checkbox"
+                  value="supernova"
+                  checked={teams.includes("סופרנובה")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                סופרנובה
+              </label>
+              <br />
+              <label>
+                <input
+                  type="checkbox"
+                  value="switch"
+                  checked={teams.includes("סוויטץ'")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                סוויטץ'
+              </label>
+              <br />
+              <label>
+                <input
+                  type="checkbox"
+                  value="starlight"
+                  checked={teams.includes("סטארלייט")}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setTeams((prev) =>
+                      prev.includes(value)
+                        ? prev.filter((v) => v !== value)
+                        : [...prev, value]
+                    );
+                  }}
+                />
+                סטארלייט
+              </label>
+            </div>
+          </div>
+
+
+          <div>
+            <Typography variant="h4">תיאור התקלה</Typography>
+            <div>
+              <label htmlFor="teamsInvolved">תיאור התקלה:</label>
+              <input id="teamsInvolved" type="text" value={errorDescription} onChange={(e) => setErrorDescription(e.target.value)} required />
+            </div>
+          </div>
+
+          <div>
+            <Typography variant="h6">פירוט התקלה</Typography>
+          </div>
+
+          <div>
+            <Typography variant="h6">פתרון התקלה</Typography>
+          </div>
+
+          <div>
+            <Typography variant="h6">סיכום אירוע</Typography>
+          </div>
+
+          <div>
+            <label htmlFor="title">מסקנות להמשך</label>
+            <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+          </div>
+
+          <button type="submit">Save Data</button>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
     </div>
   );
 };
--- frontend/src/components/pages/new_debriefing/newDebriefingStyles.ts
+++ frontend/src/components/pages/new_debriefing/newDebriefingStyles.ts
@@ -1,6 +1,32 @@
 import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()(() => ({
+    wholePageContainer: {
+        display: "flex",
+        flex: 1,
+        justifyContent: "center",
+        width: "100%",
+        backgroundColor: "#0F1027",
+        fontFamily: '"Noto Sans Hebrew", sans-serif',
+    },
+
+    allPartsContainer: {
+        display: "flex",
+        backgroundColor: "#6f0f4567",
+        flexDirection: "column",
+        gap: "15%",
+        color: "white",
+        "& ,fading-border-element": {
+            "&,fading-shadow-element": {
+                boxShadow: "0 0 20px 5px rgba(0, 0, 0, 0.5)" /* Fades a black shadow outwards */
+            }
+        }
+    },
+
+    text: {
+        fontFamily: '"Noto Sans Hebrew", sans-serif',
+    },
+
 }));
 
 export default useStyles;
