3d9cf2b4fba1fc9c777475ff2672120982f10d5a
Michal Leibovich <michalleibovich1@gmail.com>
2026-02-13 14:19:52 +0200

    added in recent debriefings buttons of filtering and sorting

--- frontend/src/components/pages/recent_debriefings/recentDebriefings.tsx
+++ frontend/src/components/pages/recent_debriefings/recentDebriefings.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import RecentDebriefingsSection from "./recent_debriefings_section/recentDebriefingsSection";
 import useStyles from "./recentDebriefingsStyles";
 
@@ -8,6 +8,21 @@ const RecentDebriefings: React.FC = () => {
         <div className={classes.wholePage}>
             <div className={classes.allPartsContainer}>
                 <Typography className={classes.text} variant="h4">תחקירים אחרונים</Typography>
+                <div className={classes.filteringSortingSearchSection}>
+                    <div className={classes.sortingSection}>
+                        <Typography className={classes.text} variant="h6">מיון לפי:</Typography>
+                        <div className={classes.sortingButtons}>
+                            <Button className={classes.button} variant="contained">זמן עדכון אחרון</Button>
+                            <Button className={classes.button} variant="contained">זמן יצירה</Button>
+                        </div>
+                    </div>
+                    <div className={classes.filteringSection}>
+                        <Button className={classes.button} variant="contained">סינון לפי תהליכים בתהליך</Button>
+                    </div>
+                    <div>
+                        <Button variant="contained">חיפוש- בהמשך</Button>
+                    </div>
+                </div>
                 <RecentDebriefingsSection />
             </div>
         </div>
--- frontend/src/components/pages/recent_debriefings/recentDebriefingsStyles.ts
+++ frontend/src/components/pages/recent_debriefings/recentDebriefingsStyles.ts
@@ -6,24 +6,51 @@ const useStyles = makeStyles()(() => ({
         flex: 1,
         justifyContent: "center",
         width: "100%",
-        backgroundColor: "#0F1027"
+        backgroundColor: "#0F1027",
     },
 
     allPartsContainer: {
         display: "flex",
         // backgroundColor: "yellow",
         flexDirection: "column",
-        gap: "15%",
+        gap: "5%",
         color: "white",
         fontFamily: '"Noto Sans Hebrew", sans-serif',
         width: "90%",
         marginTop: "20px"
     },
 
-    text: {
-        fontFamily: '"Noto Sans Hebrew", sans-serif',
+    filteringSortingSearchSection: {
+        display: "flex",
+        // backgroundColor: "pink",
+        width: "100%",
+        justifyContent: "space-between"
+    },
+
+    sortingSection: {
+        display: "flex",
+        gap: "5vw"
+    },
+
+    sortingButtons: {
+        display: "flex",
+        gap: "1vw"
+    },
+
+    filteringSection: {
+    },
+
+    button: {
+        backgroundColor: "#6f0f4582",
+        "&:hover": {
+            backgroundColor: "#98306b7f",
+        },
+    },
+
+text: {
+    fontFamily: '"Noto Sans Hebrew", sans-serif',
         textAlign: "center"
-    }
+}
 
 }));
 
